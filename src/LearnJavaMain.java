6e54270547acb44907a16fc5a533605a4a531366
Sachin Avinaw <sachinavinaw@gmail.com>
2024-06-30 19:19:21 +0900

    Update LearnJavaMain.java

--- src/LearnJavaMain.java
+++ src/LearnJavaMain.java
@@ -1,5 +1,13 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class LearnJavaMain {
     public static void main(String[] args) {
         System.out.println("Application Started..");
+
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Do you want to exit? Press Y to exit...");
+        String choice=sc.next();
+
     }
 }
